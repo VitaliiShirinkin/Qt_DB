3e3d862686d6b0d15cccc2af3a96c7b7451fec66
Vitalii Shirinkin <artoiv2@mail.ru>
2024-01-31 19:36:33 +0300

    first commit

---
+++ database.cpp
@@ -0,0 +1,115 @@
+#include "database.h"
+#include "qsqlerror.h"
+
+DataBase::DataBase(QObject *parent)
+    : QObject{parent}
+{
+    dataBase = new QSqlDatabase();
+}
+
+DataBase::~DataBase()
+{
+    delete dataBase;
+}
+
+/*!
+ * \brief Метод добавляет БД к экземпляру класса QSqlDataBase
+ * \param driver драйвер БД
+ * \param nameDB имя БД (Если отсутствует Qt задает имя по умолчанию)
+ */
+void DataBase::AddDataBase(QString driver, QString nameDB)
+{
+    *dataBase = QSqlDatabase::addDatabase(driver, nameDB);
+}
+
+/*!
+ * \brief Метод подключается к БД
+ * \param для удобства передаем контейнер с данными необходимыми для подключения
+ * \return возвращает тип ошибки
+ */
+void DataBase::ConnectToDataBase(QVector<QString> data)
+{
+    dataBase->setHostName(data[hostName]);
+    dataBase->setDatabaseName(data[dbName]);
+    dataBase->setUserName(data[login]);
+    dataBase->setPassword(data[pass]);
+    dataBase->setPort(data[port].toInt());
+
+    ///Тут должен быть код ДЗ
+
+    bool status;
+    status = dataBase->open();
+    if (status){
+        tableModel = new QSqlTableModel(this, *dataBase);
+        queryModel = new QSqlQueryModel(this);
+    }
+    emit sig_SendStatusConnection(status);
+}
+/*!
+ * \brief Метод производит отключение от БД
+ * \param Имя БД
+ */
+void DataBase::DisconnectFromDataBase(QString nameDb)
+{
+    *dataBase = QSqlDatabase::database(nameDb);
+    dataBase->close();
+}
+/*!
+ * \brief Метод формирует запрос к БД.
+ * \param request - SQL запрос
+ * \return
+ */
+void DataBase::RequestToDB(QTableView* tb_result, const requestType filtr)
+{
+    ///Тут должен быть код ДЗ
+
+    if (filtr == requestAllFilms) {
+        tableModel->setTable("film");
+        tableModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
+        tableModel->setHeaderData(1, Qt::Horizontal, tr("Название"));
+        tableModel->setHeaderData(2, Qt::Horizontal, tr("Описание"));
+        tableModel->select();
+        tb_result->setModel(tableModel);
+
+        for (int col = 0; col < tableModel->columnCount(); ++col) {
+            if (col != 1 && col != 2) { // Скрыть все столбцы, кроме второго и третьего
+                tb_result->setColumnHidden(col, true);
+            }
+        }
+        qDebug() << "Request has been sended";
+        qDebug() << "DataBase: " << tableModel->database();
+        qDebug() << "Table: " << tableModel->tableName();
+    }
+    else {
+        QString request = "SELECT title, description FROM film f "
+                                      "JOIN film_category fc on f.film_id = fc.film_id "
+                                      "JOIN category c on c.category_id = fc.category_id ";
+
+        if  (filtr == requestComedy) {
+            request = request + " WHERE c.name = 'Comedy'";
+        }
+        else {
+            request = request + " WHERE c.name = 'Horror'";
+        }
+        queryModel->setQuery(request, *dataBase);
+        qDebug() << queryModel->lastError();
+        qDebug() << queryModel->query().lastQuery();
+        queryModel->setHeaderData(0, Qt::Horizontal, tr("Название"));
+        queryModel->setHeaderData(1, Qt::Horizontal, tr("Описание"));
+        tb_result->setModel(queryModel);
+        for (int col = 0; col < queryModel->columnCount(); ++col) {
+            if (col != 0 && col != 1) { // Скрыть все столбцы, кроме второго и третьего
+                tb_result->setColumnHidden(col, true);
+            }
+        }
+    }
+    tb_result->resizeColumnsToContents();
+}
+
+/*!
+ * @brief Метод возвращает последнюю ошибку БД
+ */
+QSqlError DataBase::GetLastError()
+{
+    return dataBase->lastError();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,139 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QStandardItemModel>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    //Исходное состояние виджетов
+    ui->setupUi(this);
+    ui->lb_statusConnect->setStyleSheet("color:red");
+    ui->pb_request->setEnabled(false);
+
+    /*
+     * Выделим память под необходимые объекты. Все они наследники
+     * QObject, поэтому воспользуемся иерархией.
+    */
+
+    dataDb = new DbData(this);
+    dataBase = new DataBase(this);
+    msg = new QMessageBox(this);
+
+    //Установим размер вектора данных для подключения к БД
+    dataForConnect.resize(NUM_DATA_FOR_CONNECT_TO_DB);
+
+    /*
+     * Добавим БД используя стандартный драйвер PSQL и зададим имя.
+    */
+    dataBase->AddDataBase(POSTGRE_DRIVER, DB_NAME);
+
+    /*
+     * Устанавливаем данные для подключениея к БД.
+     * Поскольку метод небольшой используем лямбда-функцию.
+     */
+    connect(dataDb, &DbData::sig_sendData, this, [&](QVector<QString> receivData){
+        dataForConnect = receivData;
+    });
+
+
+    /*
+     *  Сигнал для подключения к БД
+     */
+    connect(dataBase, &DataBase::sig_SendStatusConnection, this, &MainWindow::ReceiveStatusConnectionToDB);
+    //connect(dataBase, &DataBase::sig_SendStatusRequest, this, &MainWindow::ReceiveStatusRequestToDB);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_act_addData_triggered()
+{
+    //Отобразим диалоговое окно.
+    dataDb->show();
+}
+
+
+
+void MainWindow::on_act_connect_triggered()
+{
+    /*
+     * Обработчик кнопки у нас должен подключаться и отключаться от БД.
+     * Можно привязаться к надписи лейбла статуса. Если он равен
+     * "Отключено" мы осуществляем подключение, если "Подключено" то
+     * отключаемся
+    */
+
+    if(ui->lb_statusConnect->text() == "Отключено"){
+
+       ui->lb_statusConnect->setText("Подключение");
+       ui->lb_statusConnect->setStyleSheet("color : black");
+
+
+       auto conn = [&]{dataBase->ConnectToDataBase(dataForConnect);};
+       QtConcurrent::run(conn);
+
+    }
+    else{
+        dataBase->DisconnectFromDataBase(DB_NAME);
+        ui->lb_statusConnect->setText("Отключено");
+        ui->act_connect->setText("Подключиться");
+        ui->lb_statusConnect->setStyleSheet("color:red");
+        ui->pb_request->setEnabled(false);
+    }
+
+}
+
+
+void MainWindow::on_pb_request_clicked()
+{
+    requestType req = requestAllFilms;
+    switch(ui->cb_category->currentIndex()){
+    case 0:
+        req = requestAllFilms;
+        break;
+    case 1:
+        req = requestComedy;
+        break;
+    case 2:
+        req = requestHorrors;
+        break;
+    default:
+        break;
+    }
+    ///Тут должен быть код ДЗ
+    ui->tb_result->setModel(nullptr);
+    dataBase->RequestToDB(ui->tb_result, req);
+
+}
+
+
+void MainWindow::ReceiveStatusConnectionToDB(bool status)
+{
+    if(status){
+        ui->act_connect->setText("Отключиться");
+        ui->lb_statusConnect->setText("Подключено к БД");
+        ui->lb_statusConnect->setStyleSheet("color:green");
+        ui->pb_request->setEnabled(true);
+    }
+    else{
+        dataBase->DisconnectFromDataBase(DB_NAME);
+        msg->setIcon(QMessageBox::Critical);
+        msg->setText(dataBase->GetLastError().text());
+        ui->lb_statusConnect->setText("Отключено");
+        ui->lb_statusConnect->setStyleSheet("color:red");
+        msg->exec();
+    }
+
+}
+
+
+void MainWindow::on_pb_clear_clicked()
+{
+    ui->tb_result->setModel(nullptr);
+}
+
